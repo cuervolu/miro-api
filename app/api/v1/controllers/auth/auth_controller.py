5743c5ac97613a86925bf094c213bf337fcdd85d
Cuervolu <cuervolu@protonmail.com>
2023-10-19 15:16:30 -0300

    feat(auth): Added refresh token function and saved tokens to Redis

--- app/api/v1/controllers/auth/auth_controller.py
+++ app/api/v1/controllers/auth/auth_controller.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, datetime
-from typing import Annotated, Any
+from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -8,11 +8,12 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from starlette import status
 
-from app.core.logger import logger
 from app.core.config import settings
-from ...schemas.user_schemas import CreateUserRequest, Token
+from app.core.logger import logger
 from app.db.session import db
 from app.models.user import User
+from ...schemas.user_schemas import CreateUserRequest, Token
+from app.core.redis_manager import redis_instance
 
 router = APIRouter(
     prefix='/auth',
@@ -29,6 +30,10 @@ def get_db():
         yield db_instance
     finally:
         db_instance.close()
+
+
+def get_redis():
+    return redis_instance
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
@@ -56,10 +61,20 @@ def create_access_token(username: str, uuid: str, expires_delta: timedelta):
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], database: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, database)
     logger.info(f"User {user.username} logged in")
+
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    refresh_token_expires = timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+
     token = create_access_token(user.username, str(user.id), access_token_expires)
+    refresh_token = create_access_token(user.username, str(user.id), refresh_token_expires)
+
+    _store_token(token, str(user.id), access_token_expires, redis_instance)
+    _store_refresh_token(refresh_token, str(user.id), refresh_token_expires, redis_instance)
+    logger.info(f"User {user.username} logged in")
+
     return {'access_token': token, 'token_type': 'bearer'}
 
 
@@ -84,3 +99,15 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Signature has expired")
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+
+
+def _store_token(token: str, user_id: str, access_token_expires: timedelta, redis: redis_instance):
+    redis_key = f"access_token:{user_id}"
+    redis.set(redis_key, token, ex=access_token_expires)
+    logger.info(f"Refresh token stored for user {user_id}")
+
+
+def _store_refresh_token(token: str, user_id: str, refresh_token_expires: timedelta, redis: redis_instance):
+    redis_key = f"refresh_token:{user_id}"
+    redis.set(redis_key, token, ex=refresh_token_expires)
+    logger.info(f"Refresh token stored for user {user_id}")
